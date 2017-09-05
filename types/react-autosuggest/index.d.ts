1bf29e6460b65b90ae5ccb91f96e451cb6f18f59
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-09-05 12:21:30 +0300

    [react-autosuggest] Fix default export type

--- types/react-autosuggest/index.d.ts
+++ types/react-autosuggest/index.d.ts
@@ -5,7 +5,7 @@
 // TypeScript Version: 2.3
 
 import * as React from 'react';
-declare class Autosuggest extends React.Component<any> {}
+declare class Autosuggest extends React.Component<Autosuggest.AutosuggestProps> {}
 
 export = Autosuggest;
 
